9a894cc578d7bfde42792a8c8b979c927e6d9daf
Lorenz Gaertner <lorenz.gaertner@physik.uni-muenchen.de>
2025-06-05 17:55:13 +0200

    improve day 1

--- days/day01/solution.cpp
+++ days/day01/solution.cpp
@@ -2,25 +2,24 @@
 
 bool solution(int k, const std::vector<int>& a){
     
-    for (size_t i = 0; i + 1 < a.size(); ++i) {
-        auto it = std::find(a.begin()+i+1, a.end(), k-a[i]);
-        if (it != a.end()) {
+    std::unordered_set<int> seen;
+
+    for (int num : a) {
+        if (seen.contains(k - num)) {
             message(k, a, true);
             return true;
         }
+        seen.insert(num);
     }
 
     message(k, a, false);
+    return false;
 
     return false;
 }
 
-void message(int k, const std::vector<int>& a, bool b) {
-    if (b) {
-        std::cout << k << " can be made from two values in [";
-    } else {
-        std::cout << k << " can NOT be made from two values in [";
-    }
+void message(int k, const std::vector<int>& a, bool found) {
+    std::cout << k << " can" << (found ? "" : " NOT") << " be made from two values in [";
     for (size_t i = 0; i < a.size(); ++i) {
         std::cout << a[i];
         if (i + 1 < a.size()) std::cout << ", ";
--- days/day01/solution.hpp
+++ days/day01/solution.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <unordered_set>
 
 bool solution(int k, const std::vector<int>& a);
 void message(int k, const std::vector<int>& a, bool b);
